733a1041c24851ab4fa7bffa636c1c0e4a524779
Lucas Gomes <lukas_g_s_@hotmail.com>
2024-10-23 18:42:49 -0300

    refactor: refactors items in array question

--- src/main/java/org/commonInterviewQuestions/FindCommonItemsInArrays.java
+++ src/main/java/org/commonInterviewQuestions/FindCommonItemsInArrays.java
@@ -1,5 +1,9 @@
 package org.commonInterviewQuestions;
 
+import org.dataStructures.array.Array;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
